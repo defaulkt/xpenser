05767b7ee7ded0599e5ef76496518ed277ff959e
Vladimir Akimov <vakimov@tango.me>
2022-07-31 15:55:10 -0700

    feat: remove fields from loader

--- loader.py
+++ loader.py
@@ -5,17 +5,9 @@ import logging
 
 class Fileloader():
     '''Class for load from files'''
-    def __init__(self, Filepath, Fields, Header = True, Filetype = "cvs", Encoding = "utf8"):
+    def __init__(self, Filepath, Header = True, Filetype = "cvs", Encoding = "utf8"):
         self.transactions = []
         self.file_path = Filepath
-        if not isinstance(Fields, dict):
-            print("Fields must be a dictinary: {'field_name': field_number} ")
-            self.fields = {}
-        elif min(Fields.values()) < 1:
-            print("Fields positions cannot be < 1")
-            self.fields = {}
-        else:
-            self.fields = Fields
         if Filetype == "csv":
             self.divder = ","
         elif Filetype == "tsv":
@@ -33,14 +25,9 @@ class Fileloader():
         try:
             with open(self.file_path, "r", encoding=self.encoding) as file_descriptor:
                 for line in file_descriptor:
-                    transaction = {}
                     parsed_line = line.strip().split(self.divder)
                     self.logger.debug("Reading a line from the file: %s", parsed_line)
-                    if len(parsed_line) < max(self.fields.values()):
-                        pass
-                    for field_name, field_position in self.fields.items():
-                        transaction[field_name] = parsed_line[field_position-1]
-                    self.transactions.append(transaction)
+                    self.transactions.append(parsed_line)
         except IOError as io_exception:
             self.logger.critical('Operation failed: %s' , str(io_exception.strerror))
         file_descriptor.close()
